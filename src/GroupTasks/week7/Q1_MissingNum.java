e1e3e9df0d1f333f9083846e6cab2fc390c53b78
Ivan_Cydeo <remezivan1@gmail.com>
2023-03-04 15:20:03 -0500

    Week 7 tasks complete

--- src/GroupTasks/week7/Q1_MissingNum.java
+++ src/GroupTasks/week7/Q1_MissingNum.java
@@ -27,10 +27,127 @@ Follow up: Could you implement a solution using only O(1) extra space complexity
 Please, test your solution here: https://leetcode.com/problems/missing-number/
  */
 
+import java.util.Arrays;
+
 public class Q1_MissingNum {
 
     public static void main(String[] args) {
 
+        //int[] nums = {3, 0, 1};
+        int[] nums = {9, 6, 4, 2, 3, 5, 7, 0, 1};
 
+        //int[] nums = {0,1};
+        System.out.println(missingNum_Mahir(nums));
+
+        System.out.println(Oleksii_MissingNum(nums));
+
+        int[] arr = {3, 0, 1};
+        int[] arr2 = {0, 1};
+        int[] arr3 = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+        System.out.println(missingNumber_Zehra(arr)); // 2
+        System.out.println(missingNumber_Zehra(arr2)); // 2
+        System.out.println(missingNumber_Zehra(arr3)); // 8
+
+
+        int[] array = {5,3,7,2,1,9,6,0,8};
+        System.out.println(missingNumber_Emre(array));
+
+        System.out.println("-----------------------------------------------------");
+        int[] array_Sefika ={1,3,5,2,0}; //n=5 , [0,5]
+        System.out.println(Q1_MissingNum.missingNumber_Sefika(array_Sefika)); //4
+
+        int[] nums1 = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+        int size = nums1.length;
+        System.out.println(missingNum_Ivan(nums1, size));
+
+        int[] array_Malik ={1,3,5,2,0};//n=5 [0,5]
+        System.out.println(isMissing_Malik(array_Malik));
+    }
+
+    static int Oleksii_MissingNum(int[] arr) {
+
+        int k = ((arr.length ) * (arr.length+1)) / 2;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            k -= arr[i];
+        }
+        return k;
+
+    }
+
+    public static int missingNumber_Zehra(int[] array) {
+
+        int result = 0;
+
+        int n = array.length;
+
+        for (int each : array) {
+            result += each;
+        }
+
+        int sumOfArray = n * (n + 1) / 2;
+
+        if (result == sumOfArray) {
+            return 0;
+        } else {
+            return sumOfArray - result;
+        }
+    }
+
+    public static int missingNumber_Emre(int[] array) {
+
+        Arrays.sort(array);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != i) {
+                return i;
+            }
+        }
+        return array[array.length - 1] + 1;
+    }
+    public static  int missingNum_Mahir(int[]nums){
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i!=nums[i]){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    public static int missingNumber_Sefika(int[] array_Sefika){
+        int result= 0;
+        int n = array_Sefika.length;
+        int sum = ( n * (n+1)) / 2;
+        int sumOfArray = Arrays.stream(array_Sefika).sum();
+        result = sum - sumOfArray;
+        return result;
+
+    }
+
+    public static int missingNum_Ivan(int[] nums, int n)
+    {
+        int sum = (n * (n + 1)) / 2;
+        for (int i = 0; i < n; i++)
+            sum -= nums[i];
+        return sum;
+    }
+    public static int isMissing_Malik(int[] arr){
+
+        int ar=0;
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+
+            if(ar==arr[i]){
+                ar++;
+            } else{
+                break;
+            }
+
+
+        }
+        return ar;
     }
 }
--- src/GroupTasks/week7/Q2_TwoSum.java
+++ src/GroupTasks/week7/Q2_TwoSum.java
@@ -17,10 +17,147 @@ Explanation: Because nums[1] + nums[3] => 3 + 5 = 8, we return [3, 5].
 Optional additional task: Try to return original indices of the pair numbers.
  */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Q2_TwoSum {
 
     public static void main(String[] args) {
 
+        int[] arr = {16, 3, 11, 5, 15};
+        int target = 8;
 
+        // indices:
+        System.out.println(Arrays.toString(twoSum_Ivan(arr, target)));  // [1, 3] - not sorted
+        System.out.println(Arrays.toString(TwoSum_Mahir(arr,target)));
+        System.out.println(Arrays.toString(Oleksii_TwoSum(arr, target)));
+
+        Integer[] nums = {5, 2, 3, 4, 1};
+
+        System.out.println(Arrays.toString(twoSum_Emre(nums,8)));
+
+        System.out.println(Arrays.toString(sum_Zehra(arr, 8))); // [3, 5]
+
+        System.out.println(Arrays.toString(twoSum_Malik(arr,8)));
+
+    }
+
+    public static int[] sum_Zehra(int[] arr, int target) {
+
+        Arrays.sort(arr);
+
+        int rightPointer = 0;
+        int leftPointer = arr.length - 1;
+
+        while (rightPointer < leftPointer) {
+
+            if (arr[rightPointer] + arr[leftPointer] < target) {
+                rightPointer++;
+            } else if (arr[rightPointer] + arr[leftPointer] > target) {
+                leftPointer--;
+            } else {
+                return new int[]{arr[rightPointer], arr[leftPointer]};
+            }
+        }
+        return new int[]{-1, -1};
+
+    }
+
+    static int[] Oleksii_TwoSum(int[] arr, int target) {
+
+        int leftP = 0;
+        int rightP = arr.length - 1;
+
+
+        arr = Arrays.stream(arr).sorted().toArray();
+        while (leftP < rightP) {
+            if (arr[leftP] + arr[rightP] < target) {
+                leftP++;
+                continue;
+            } else if (arr[leftP] + arr[rightP] > target) {
+                rightP--;
+                continue;
+            } else if (arr[leftP] + arr[rightP] == target) {
+                return new int[]{arr[leftP], arr[rightP]};
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static Integer[] twoSum_Emre(Integer[] nums, int target) {
+
+        Integer[] array = Arrays.copyOf(nums,nums.length);
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(array));
+
+        Arrays.sort(nums);
+
+        int left_pointer = 0;
+        int right_pointer = nums.length-1;
+
+        while (left_pointer < right_pointer) {
+
+            int pairSum = nums[left_pointer] + nums[right_pointer];
+
+            if (pairSum < target)
+                left_pointer++;
+            else if (pairSum > target)
+                right_pointer--;
+            else
+                return new Integer[] {list.indexOf(nums[left_pointer]),list.indexOf(nums[right_pointer])};
+
+        }
+        return new Integer[]{};
+    }
+    public static int[] TwoSum_Mahir(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        Arrays.sort(nums);
+        while (r > l) {
+            if (nums[r] + nums[l] == target) {
+                return new int[]{nums[l], nums[r]};
+            } else if (nums[r] + nums[l] < target)
+                l++;
+            else {
+                r--;
+            }
+        }
+        return new int[]{};
+    }
+
+    public static int[] twoSum_Ivan(int[] nums, int target) {
+
+        int[] ans = new int[2];
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(nums[0], 0);
+
+        for(int i = 1; i < nums.length; i++) {
+            int val = target - nums[i];
+            if(map.containsKey(val)) {
+                ans[0] = map.get(val);
+                ans[1] = i;
+                return ans;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return ans;
+    }
+    public static int[] twoSum_Malik(int[]ar,int target){
+        int []an=new int[2];
+
+        for (int i = 0; i < ar.length; i++) {
+            for (int j =i+1; j < ar.length; j++) {
+                if(ar[i]+ar[j]==target){
+                    an[0]+=ar[i];
+                    an[1]+=ar[j];
+                    break;
+                }
+            }
+        }
+
+        return an;
     }
 }
