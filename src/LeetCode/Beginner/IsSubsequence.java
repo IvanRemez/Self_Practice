c39a115f96c932732b24461c5892ffd5e4717ae3
Ivan_Cydeo <remezivan1@gmail.com>
2023-07-20 19:08:33 -0400

    LeetCode - update

--- src/LeetCode/Beginner/IsSubsequence.java
+++ src/LeetCode/Beginner/IsSubsequence.java
@@ -1,4 +1,5 @@
 package LeetCode.Beginner;
+
 /*
 392. Is Subsequence
 
@@ -26,23 +27,47 @@ public class IsSubsequence {
 
     public static void main(String[] args) {
 
+        String s = "abc";
+        String t = "ahbgdcg";
+
+        System.out.println(isSubsequence(s, t));
+        System.out.println(isSubsequence2(s, t));
+
 
     }
 
-//    public boolean isSubsequence(String s, String t) {
-//
-//        char[] s_char = s.toCharArray();
-//        char[] t_char = t.toCharArray();
-//
-//        for (int i = 0; i < t_char.length; i++) {
-//
-//            char[] new_char = new char[s_char.length];
-//
-//            if (t_char[i] == s_char[i]) {
-//                new_char[i] = s_char[i];
-//            }
-//        }
-//    }
+    public static boolean isSubsequence(String s, String t) {
 
+        if (s.isEmpty()) {
+            return true;
+        }
 
+        int count = 0;
+        String result = "";
+
+        for (int i = 0; i < t.length(); i++) {
+
+            if (s.charAt(count) == t.charAt(i)) {
+                result += s.charAt(count);
+                count++;
+                if ((s.length()) == count) {
+                    return result.equals(s);
+                }
+            }
+        }
+        return false;
+    }
+
+    private static int index = -1;
+    public static boolean isSubsequence2(String s, String t) {
+
+        for (int i = 0; i < s.length(); i++) {
+            index = t.indexOf(s.charAt(i), index + 1);
+            if (index == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
