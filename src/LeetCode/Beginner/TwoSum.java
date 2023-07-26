dc8a26752b4cd7664bd860dfd3c23865cafca298
Ivan_Cydeo <remezivan1@gmail.com>
2023-07-25 22:53:31 -0400

    LeetCode - update

---
+++ src/LeetCode/Beginner/TwoSum.java
@@ -0,0 +1,32 @@
+package LeetCode.Beginner;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int potentialMatch = target - nums[i];
+
+            if (map.containsKey(potentialMatch)) {
+                return new int[]{i, map.get(potentialMatch)};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
+}
